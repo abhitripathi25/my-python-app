9dd2cadab15e46c6d7c6d25b53eb57772fad1404
Abhishek Tripathi <abhiiit1234@gmail.com>
2025-08-28 14:20:02 +0530

    run script

--- app.py
+++ app.py
@@ -1,2 +1,19 @@
-print("Hello DevOps! This is my first Python + Jenkins pipeline.")
+pipeline {
+    agent any
+
+    stages {
+        stage('Checkout') {
+            steps {
+                git branch: 'abhishek',
+                    url: 'https://github.com/abhitripathi25/my-python-app.git'
+            }
+        }
+
+        stage('Run Script') {
+            steps {
+                sh 'python3 app.py'
+            }
+        }
+    }
+}
 
